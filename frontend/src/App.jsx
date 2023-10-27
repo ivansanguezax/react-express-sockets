598c36e53bbe1f01b586778641b7716981d67c8a
ivansanguezax <ivansanguezax@gmail.com>
2023-10-26 22:16:30 -0400

    [003-TailwindCSS]

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -27,25 +27,37 @@ export default function App() {
     setMessages((state) => [...state, message]);
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
+    <div className="h-screen bg-zinc-800 text-white flex items-center justify-center">
+      <form onSubmit={handleSubmit} className="bg-zinc-900 p-10">
+        <h1
+          className="
+        
+        text-3xl font-bold text-center mb-10"
+        >
+          Chat con React
+        </h1>
         <input
           onChange={(e) => setMessage(e.target.value)}
           type="text"
           placeholder="Write your message..."
           name="message"
           id="message"
+          className="border-2 border-zinc-500 p-2 rounded-md w-full mb-5 text-black"
         />
-        <button type="submit">Send</button>
-      </form>
-
-      <ul>
+        <ul>
         {messages.map((message, index) => (
-          <li key={index}>
-            {message.from}:{message.body}
+          <li key={index} className={
+            message.from === "Me" ? "my-2 p-2 table text-sm rounded-md bg-sky-500 mr-auto" : "my-2 p-2 table text-sm rounded-md bg-green-700 ml-auto"
+          }
+          >
+            <span className="text-xs text-white-200 block">{message.from}: </span>
+            <span className="text-md">{message.body}</span>
           </li>
         ))}
       </ul>
+      </form>
+
+      
     </div>
   );
 }
