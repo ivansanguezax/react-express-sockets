40c8f81a76b6e6e23d48d49f96dee52ebaa0684c
ivansanguezax <ivansanguezax@gmail.com>
2023-10-26 22:00:52 -0400

    [002-Sending-Messages-Websockets]

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,51 @@
+import { useState, useEffect } from "react";
 import io from "socket.io-client";
 
-const socket = io('/');
+const socket = io("/");
 
 export default function App() {
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newMessage = {
+      from: "Me",
+      body: message,
+    };
+    setMessages([...messages, newMessage]);
+    socket.emit("message", message);
+  };
+  useEffect(() => {
+    socket.on("message", receiveMessage);
+
+    return () => {
+      socket.off("message", receiveMessage);
+    };
+  }, []);
+
+  const receiveMessage = (message) =>
+    setMessages((state) => [...state, message]);
+
   return (
     <div>
-      Hello World
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={(e) => setMessage(e.target.value)}
+          type="text"
+          placeholder="Write your message..."
+          name="message"
+          id="message"
+        />
+        <button type="submit">Send</button>
+      </form>
+
+      <ul>
+        {messages.map((message, index) => (
+          <li key={index}>
+            {message.from}:{message.body}
+          </li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
--- server/index.js
+++ server/index.js
@@ -7,7 +7,15 @@ const server = http.createServer(app);
 const io = new SocketServer(server);
 
 io.on("connection", (socket) => {
-  console.log("a user connected");
+  console.log(socket.id, "connected");
+  
+  socket.on("message", (body) => {
+    console.log(body);
+    socket.broadcast.emit("message", {
+      body,
+      from: socket.id.slice(6),
+    });
+  });
 });
 
 server.listen(3000);
